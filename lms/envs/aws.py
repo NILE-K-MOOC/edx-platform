e303af1b9cc8762ffd68a73bc57e1a8c9674598e
redukyo <redukyo@naver.com>
2016-12-18 10:00:26 +0000

    third party auth 설정 및 sass 수정

--- lms/envs/aws.py
+++ lms/envs/aws.py
@@ -634,7 +634,7 @@ if FEATURES.get('ENABLE_THIRD_PARTY_AUTH'):
             'third_party_auth.naver.NaverOAuth2',
             'third_party_auth.lifelongedu.LifelongeduOAuth2',
             'third_party_auth.allprovider.LifelongeduOAuth2',
-            'social.backends.google.kakao',
+            'social.backends.kakao.KakaoOAuth2',
             'social.backends.google.GoogleOAuth2',
             'social.backends.linkedin.LinkedinOAuth2',
             'social.backends.facebook.FacebookOAuth2',
