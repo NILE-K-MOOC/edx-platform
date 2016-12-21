dd59d1002a1270af3ae9e019924721e6412b3a64
신영규 <antshin72@gmail.com>
2016-12-22 04:48:35 +0900

    CDN 업로드 top 메뉴 토클 패치함

--- cms/djangoapps/contentstore/features/upload.py
+++ cms/djangoapps/contentstore/features/upload.py
@@ -24,9 +24,9 @@ def go_to_uploads(_step):
     world.css_click(uploads_css)
 
 @step(u'I go to the files and uploads_cdn page$')
-def go_to_uploads(_step):
+def go_to_uploads_cdn(_step):
     menu_css = 'li.nav-course-courseware'
-    uploads_css = 'li.nav-course-courseware-uploads_cdn a'
+    uploads_css = 'li.nav-course-courseware-uploads-cdn a'
     world.css_click(menu_css)
     world.css_click(uploads_css)
 
