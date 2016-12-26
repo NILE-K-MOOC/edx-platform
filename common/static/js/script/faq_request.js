cbfa97046cb8ff38b988281418e3700c8d96170c
kotechkice <coke1541@kotech.co.kr>
2016-12-26 17:03:35 +0900

    문의하기 직접입력 추가

--- common/static/js/script/faq_request.js
+++ common/static/js/script/faq_request.js
@@ -1,18 +1,31 @@
 /**
  * Created by dev on 2016. 11. 21..
  */
+
 $(document).ready(function(){
 
+});
+
+$(document).on('change', '#sel_email', function(){
+    if($('#sel_email option:selected').attr('id') != 'direct' && $('#sel_email option:selected').attr('id') != 'null'){
+        $('#email_txt').attr('disabled', 'true');
+    }else if($('#sel_email option:selected').attr('id') == 'direct'){
+        $('#email_txt').removeAttr('disabled').focus();
+    }
 });
 
 $(document).on('click', '#request', function(){
     var regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
     var flag = '';
-    var email = $('#email').val()+'@'+$('#sel_email option:selected').val();
-    var option = $('#search_con option:selected').attr('id');
-    var request_con = $('#request_con').val();
-
-
+    var sel_email = '';
+    var email = '';
+    if($('#sel_email option:selected').attr('id') != 'direct' && $('#sel_email option:selected').attr('id') != 'null'){
+        sel_email = $('#sel_email option:selected').val();
+        email = $('#email').val()+'@'+sel_email;
+    }else if($('#sel_email option:selected').attr('id') == 'direct'){
+        sel_email = $('#email_txt').val();
+        email = $('#email').val()+'@'+$('#email_txt').val();
+    }
 
     if(email.match(regExp) != null){
         flag = true;
@@ -21,6 +34,8 @@ $(document).on('click', '#request', function(){
     }
 
 
+    var option = $('#search_con option:selected').attr('id');
+    var request_con = $('#request_con').val();
     if(option != 'null' && flag == true){
         $('#request').attr('disabled', 'true');
         $.ajax({
@@ -37,6 +52,7 @@ $(document).on('click', '#request', function(){
                 location.href='/comm_faq/1/'
             }else{
                 alert('문의하기가 정상적으로 되지않았습니다. 잠시 후에 시도해주세요.');
+                location.href='/comm_faq/1/'
             }
         });
     }
@@ -46,8 +62,6 @@ $(document).on('click', '#request', function(){
     else{
         alert('문의 내용의 종류를 선택해주세요.');
     }
-
-
 });
 
 
