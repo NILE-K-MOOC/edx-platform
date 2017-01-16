efe2d7e4435228e7d8b49e3b9fad75234a076f44
kotechkice <coke1541@kotech.co.kr>
2017-01-16 17:23:35 +0900

    파일경로 replace 정규식 변경

--- common/static/js/script/mod_knews.js
+++ common/static/js/script/mod_knews.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
         console.log(data);
         var title = data[3]+data[0];
         $('#title').html(title);
-        data[1] = data[1].replace("/manage/home/static/upload/",'/static/file_upload/');
+        data[1] = data[1].replace(/\/manage\/home\/static\/upload\//g,'/static/file_upload/');
         $('#context').html(data[1].replace(/\&\^\&/g, ','));
         $('#reg_date').html('작성일 : '+data[2]);
         if(data[4] != '' && data[4] != null){
