7ef68488a3b9bdb012f38b78844b18b223a2cd27
redukyo <redukyo@naver.com>
2017-08-21 09:19:10 +0000

    선관위 프로바이더 수정

--- common/djangoapps/third_party_auth/necprovider.py
+++ common/djangoapps/third_party_auth/necprovider.py
@@ -1,5 +1,4 @@
 from social.backends.oauth import BaseOAuth2
-from django.utils.http import urlencode
 import json
 
 
@@ -9,13 +8,7 @@ class OAuth2(BaseOAuth2):
     # callback name
     name = 'nec'
     AUTHORIZATION_URL = 'http://www.civicedu.go.kr/web/oauth/server/authorize.jsp'
-    # ACCESS_TOKEN_URL = 'http://www.civicedu.go.kr/web/oauth/server/token.jsp'
-
-    # test
-    ACCESS_TOKEN_URL = 'http://www.civicedu.go.kr/web/oauth/server/ttt.jsp'
-
-    # AUTHORIZATION_URL = 'http://myserver:8002/o/authorize'
-    # ACCESS_TOKEN_URL = 'http://myserver:8002/o/token/'
+    ACCESS_TOKEN_URL = 'http://www.civicedu.go.kr/web/oauth/server/token2.jsp'
     SCOPE_SEPARATOR = ','
     ACCESS_TOKEN_METHOD = 'POST'
     EXTRA_DATA = [
@@ -26,39 +19,28 @@ class OAuth2(BaseOAuth2):
 
     def get_user_details(self, response):
 
-        print 'get_user_details called!!'
+        # print 'response ---------------- s'
+        # print response
+        # print 'response ---------------- e'
 
         """Return user details from Github account"""
-        #        return {'username': response.get('login'),
-        #                'email': response.get('email') or '',
-        #                'first_name': response.get('name'),
-        #                'fullname': response.get('name'),
-        #                'last_name': response.get('name'),
-        #                }
-
-        jsonString = "{"
-        jsonString += '"login":"' + response.get('login') + '"'
-        jsonString += ',"email":"' + response.get('email') + '"'
-        jsonString += ',"name":"' + response.get('name') + '"'
-        jsonString += ',"id":"' + response.get('id') + '"'
-        jsonString += '}'
+        jsonString = dict()
+        jsonString['login'] = response.get('user_id')
+        jsonString['email'] = response.get('email')
+        jsonString['name'] = response.get('name')
+        jsonString['id'] = response.get('id')
 
         print '***********************'
         print jsonString
         print '***********************'
 
-        return json.loads(jsonString)
+        return json.loads(json.dumps(jsonString))
 
     def user_data(self, access_token, *args, **kwargs):
         """Loads user data from service"""
         print 'user_data called!!'
 
-        url = 'http://www.civicedu.go.kr/web/oauth/server/resource.jsp?' + urlencode({
-            # url = 'http://myserver:8002/api/hello?' + urlencode({
-            'access_token': access_token
-        })
-
-        str = self.get_json(url)
+        str = self.get_json('http://www.civicedu.go.kr/web/oauth/server/resource2.jsp', params={'access_token': access_token})
 
         try:
             return str
@@ -73,16 +55,6 @@ class OAuth2(BaseOAuth2):
         print self.auth_headers()
         print self.ACCESS_TOKEN_METHOD
         print 'auth_complete called2'
-
-        # if 'access_token' in self.data and 'code' not in self.data:
-        #     raise AuthMissingParameter(self, 'access_token or code')
-        # # Token won't be available in plain server-side workflow
-        # token = self.data.get('access_token')
-        # if token:
-        #     self.process_error(self.get_json(
-        #         'https://www.googleapis.com/oauth2/v1/tokeninfo',
-        #         params={'access_token': token}
-        #     ))
 
         response = self.request_access_token(
             self.ACCESS_TOKEN_URL,
