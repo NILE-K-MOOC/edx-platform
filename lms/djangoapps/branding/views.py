643893316786eccc2dcdf5478482ed5f31fc4b16
ahn <93immm@naver.com>
2017-11-01 01:16:12 +0000

    api 임시 저장

--- lms/djangoapps/branding/views.py
+++ lms/djangoapps/branding/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*- 
 """Views for the branding app. """
 import logging
 import urllib
@@ -6,7 +7,7 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.core.cache import cache
 from django.views.decorators.cache import cache_control
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from django.utils import translation
 from django.shortcuts import redirect
 from django.views.decorators.csrf import ensure_csrf_cookie
@@ -26,8 +27,313 @@ import sys
 import json
 import MySQLdb as mdb
 
+from django.db import connections
+import pymysql
+import pymongo
+from pymongo import MongoClient
+from bson import ObjectId
+import re
+
 log = logging.getLogger(__name__)
 
+def get_univ_name(univ_code):
+    if univ_code == 'KYUNGNAMUNIVk':
+        return '경남대학교'
+    elif univ_code == 'KHUk':
+        return '경희대학교'
+    elif univ_code == 'KoreaUnivK':
+        return '고려대학교'
+    elif univ_code == 'DGUk':
+        return '대구대학교'
+    elif univ_code == 'PNUk':
+        return '부산대학교'
+    elif univ_code == 'SMUCk':
+        return '상명대학교(천안)'
+    elif univ_code == 'SMUk':
+        return '상명대학교(서울)'
+    elif univ_code == 'SNUk':
+        return '서울대학교'
+    elif univ_code == 'SKKUk':
+        return '성균관대학교'
+    elif univ_code == 'KKUk':
+        return '건국대학교(글로컬)'
+    elif univ_code == 'SSUk':
+        return '성신여자대학교'
+    elif univ_code == 'SejonguniversityK':
+        return '세종대학교'
+    elif univ_code == 'SookmyungK':
+        return '숙명여자대학교'
+    elif univ_code == 'YSUk':
+        return '연세대학교'
+    elif univ_code == 'YeungnamUnivK':
+        return '영남대학교'
+    elif univ_code == 'UOUk':
+        return '울산대학교'
+    elif univ_code == 'EwhaK':
+        return '이화여자대학교'
+    elif univ_code == 'INHAuniversityK':
+        return '인하대학교'
+    elif univ_code == 'CBNUk':
+        return '전북대학교'
+    elif univ_code == 'KonkukK':
+        return '건국대학교'
+    elif univ_code == 'KSUk':
+        return '경성대학교'
+    elif univ_code == 'SOGANGk':
+        return '서강대학교'
+    elif univ_code == 'POSTECHk':
+        return '포항공과대학교'
+    elif univ_code == 'KAISTk':
+        return '한국과학기술원'
+    elif univ_code == 'HYUk':
+        return '한양대학교'
+    elif univ_code == 'HongikUnivK':
+        return '홍익대학교'
+    elif univ_code == 'CAUk':
+        return '중앙대학교'
+    elif univ_code == 'GachonUnivK':
+        return '가천대학교'
+    elif univ_code == 'DonggukK':
+        return '동국대학교'
+    elif univ_code == 'DSUk':
+        return '동신대학교'
+    elif univ_code == 'DAUk':
+        return '동아대학교'
+    elif univ_code == 'TUk':
+        return '동명대학교'
+    elif univ_code == 'MokwonK':
+        return '목원대학교'
+    elif univ_code == 'PCUk':
+        return ' 배재대학교'
+    elif univ_code == 'PKNUk':
+        return '부경대학교'
+    elif univ_code == 'SunMoonK':
+        return '선문대학교'
+    elif univ_code == 'SCHk':
+        return '순천향대학교'
+    elif univ_code == 'WSUK':
+        return '우송대학교'
+    elif univ_code == 'WKUk':
+        return '원광대학교'
+    elif univ_code == 'InhatcK':
+        return '인하공업전문대학교'
+    elif univ_code == 'ChosunK':
+        return ' 조선대학교'
+    elif univ_code == 'CNUk':
+        return '충남대학교'
+    elif univ_code == 'ChungbukK':
+        return '충북대학교'
+    elif univ_code == 'KonYangK':
+        return '건양대학교'
+    elif univ_code == 'UOSk':
+        return '서울시립대학교'
+    elif univ_code == 'SoongsilUnivK':
+        return '숭실대학교'
+    elif univ_code == 'JNUk':
+        return '전남대학교'
+    elif univ_code == 'CKUk':
+        return '가톨릭관동대학교'
+    elif univ_code == 'HallymK':
+        return '한림대학교'
+    elif univ_code == 'KONGJUk':
+        return '공주대학교'
+    elif univ_code == 'SEOULTECHk':
+        return '서울과학기술대학교'
+    elif univ_code == 'SKUk':
+        return '성결대학교'
+    elif univ_code == 'AYUk':
+        return '안양대학교'
+    elif univ_code == 'YonseiWK':
+        return '연세대학교(원주)'
+    elif univ_code == 'HansungK':
+        return '한성대학교'
+    elif univ_code == 'KUMOHk':
+        return '금오공과대학교'
+    elif univ_code == 'DKUK':
+        return '단국대학교'
+    elif univ_code == 'BUFSk':
+        return '부산외국어대학교'
+    elif univ_code == 'SWUk':
+        return '서울여자대학교'
+    elif univ_code == 'SYUk':
+        return '삼육대학교'
+    elif univ_code == 'KNUk':
+        return '경북대학교'
+    elif univ_code == 'KIUk':
+        return '경일대학교'
+    elif univ_code == 'KMUk':
+        return '국민대학교'
+    elif univ_code == 'DCUk':
+        return '대구가톨릭대학교'
+    elif univ_code == 'DHUk':
+        return '대구한의대학교'
+    elif univ_code == 'DJUk':
+        return '대전대학교'
+    elif univ_code == 'CUKk':
+        return '가톨릭대학교'
+    elif univ_code == 'JEJUk':
+        return '제주대학교'
+    elif univ_code == 'HGUk':
+        return '한동대학교'
+    elif univ_code == 'SKP.KAISTk':
+        return '서울대, 한국과학기술원, 포항공대'
+    elif univ_code == 'SKP.SNUk':
+        return '서울대, 한국과학기술원, 포항공대'
+    elif univ_code == 'SKP.POSTECHk':
+        return '서울대, 한국과학기술원, 포항공대'
+    elif univ_code == 'SKP_KAISTk':
+        return '서울대, 한국과학기술원, 포항공대'
+    elif univ_code == 'SKP_SNUk':
+        return '서울대, 한국과학기술원, 포항공대'
+    elif univ_code == 'SKP_POSTECHk':
+        return '서울대, 한국과학기술원, 포항공대'
+    else:
+        return '알수없음'
+
+def course_api(request):
+    conn = pymysql.connect(host='192.168.33.21', user='hello', password='0000',
+                       db='edxapp', charset='utf8')
+    cur = conn.cursor()
+    #with connections['default'].cursor() as cur:
+    sql = '''
+    SELECT id, 
+           display_name, 
+           start, 
+           end, 
+           enrollment_start, 
+           enrollment_end, 
+           created, 
+           modified, 
+           course_video_url, 
+           course_image_url 
+    FROM   edxapp.course_overviews_courseoverview
+    '''
+    cur.execute(sql)
+    slist = cur.fetchall()
+
+    # ----- mongo ----- #
+    m_org = 'POSTECHk'
+    m_course = 'POSTECH.EECE341k'
+    m_run = '2015-01'
+
+    client = MongoClient('192.168.33.21', 27017)
+    db = client["edxapp"]
+    collection = db["modulestore.active_versions"]
+
+    cursor = collection.find({'org':m_org, 'course':m_course, 'run':m_run})
+
+    pb = str(cursor[0]['versions']['published-branch'])
+
+    print pb #DEBUG
+
+    collection = db["modulestore.structures"]
+    cursor = collection.find({'_id':ObjectId(pb)})
+
+    block_text = str(cursor[0]['blocks'])
+
+    index = block_text.find("u'block_type': u'course'")
+    course_text = block_text[index:]
+
+    ct_start = course_text.find("u'children':")
+    ct_end = course_text.find("u'linguistics'")
+
+    children = course_text[ct_start:ct_end-2]
+    children = children.replace('[','{')
+    children = children.replace(']','}')
+ 
+    print children #DEBUG
+
+    #cc = children.count('chapter')
+ 
+    chapter_list = []
+
+    for m in re.finditer('chapter', children):
+        chapter_list.append( children[ m.start()+12 : m.end()+37 ] )
+
+    index = block_text.find(chapter_list[0])
+    course_text = block_text[index: index+5000]
+    dn_index = course_text.find("u'display_name'")
+    dn = course_text[dn_index:dn_index+500]
+    
+    print "#########################"
+
+    print dn
+
+    print "#########################"
+
+    #for i in chapter_list:
+    #    print i
+    #print len(chapter_list)
+    #print children.find("u'",0)
+    #print children.find("u'",1)
+
+    #print cursor
+    #cursor = collection.find({'versions':{'published-branch':pb}})
+    #for document in cursor:
+    #    print document
+
+    # ----- mongo ----- #
+
+    # init list
+    display_name_list = []
+    course_id_list = []
+    univ_name_list = []
+    start_time_list = []
+    end_time_list = []
+    enroll_start_list = []
+    enroll_end_list = []
+    created_list = []
+    modified_list = []
+    video_list = []
+    img_list = []
+    location_list = []
+
+    for item in slist:
+        # making
+        course_id = item[0]
+        start = course_id.find(':')
+        end = course_id.find('+')
+        univ_code = course_id[ start+1 : end ]
+        univ_name = get_univ_name(univ_code)
+        # insert
+        univ_name_list.append(univ_name)
+        course_id_list.append(course_id)
+
+    for item in slist:
+        # insert
+        display_name_list.append(item[1])
+        start_time_list.append(item[2])
+        end_time_list.append(item[3])
+        enroll_start_list.append(item[4])
+        enroll_end_list.append(item[5])
+        created_list.append(item[6])
+        modified_list.append(item[7])
+        video_list.append(item[8])
+        img_list.append("http://www.kmooc.kr" + str(item[9]))
+
+    # ----- DEBUG ----- #
+    print "##############"
+    print "total = {}".format(len(slist))
+    """
+    for n in range(0, len(slist) ):
+        print "--------------------- {}".format(n)
+        print course_id_list[n]
+        print display_name_list[n]
+        print univ_name_list[n]
+        print start_time_list[n]
+        print end_time_list[n]
+        print enroll_start_list[n]
+        print enroll_end_list[n]
+        print created_list[n]
+        print modified_list[n]
+        print video_list[n]
+        print img_list[n]
+        print "---------------------"
+    """
+    print "##############"
+    # ----- DEBUG ----- #
+
+    return JsonResponse({'foo':slist})
 
 def get_course_enrollments(user):
     """
