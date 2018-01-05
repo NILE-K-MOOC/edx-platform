190bf191180d607ea56fee9249930b80600c8dc0
redukyo <redukyo@naver.com>
2018-01-05 05:05:26 +0000

    defence xss (add tags)

--- lms/static/js/Markdown.Editor.js
+++ lms/static/js/Markdown.Editor.js
@@ -996,6 +996,9 @@
             console.debug('pushPreviewHtml.');
 
             text = String(text).replace(/script/ig, '_script');
+            text = String(text).replace(/iframe/ig, '_iframe');
+            text = String(text).replace(/xmp/ig, '_xmp');
+            text = String(text).replace(/xml/ig, '_xml');
             text = String(text).replace(/on/ig, '_on');
 
             var emptyTop = position.getTop(panels.input) - getDocScrollTop();
