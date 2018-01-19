63a7db8d6bea0288933433ca921760c653b07149
redukyo <redukyo@naver.com>
2018-01-19 06:12:57 +0000

    게시판 상세보기시 첨부파일 검색 조건 수정

--- lms/djangoapps/community/views.py
+++ lms/djangoapps/community/views.py
@@ -118,7 +118,7 @@ def comm_view(request, board_id=None):
     board = TbBoard.objects.get(board_id=board_id)
 
     if board:
-        board.files = TbBoardAttach.objects.filter(board_id=board_id)
+        board.files = TbBoardAttach.objects.filter(board_id=board_id, del_yn='N')
 
     section = board.section
 
