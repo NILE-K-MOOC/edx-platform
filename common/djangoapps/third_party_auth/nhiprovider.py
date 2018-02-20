de5e58a5fd236a6a3400255cd7a734ec1740f85c
redukyo <redukyo@naver.com>
2018-02-20 06:11:11 +0000

    인재개발원 프로바이더 개발 완료

--- common/djangoapps/third_party_auth/nhiprovider.py
+++ common/djangoapps/third_party_auth/nhiprovider.py
@@ -1,14 +1,13 @@
-from social.backends.oauth import BaseOAuth2
 import json
+
+from social.backends.oauth import BaseOAuth2
 
 
 class OAuth2(BaseOAuth2):
-    """civicedu OAuth authentication backend"""
-
     # callback name
     name = 'nhi'
     AUTHORIZATION_URL = 'http://e-learning.nhi.go.kr/external/kmooc.do'
-    ACCESS_TOKEN_URL = 'https://policy.nhi.go.kr/oauth2/authoriza.do'
+    ACCESS_TOKEN_URL = 'https://policy.nhi.go.kr/oauth2/token.do'
     SCOPE_SEPARATOR = ','
     ACCESS_TOKEN_METHOD = 'POST'
     EXTRA_DATA = [
@@ -18,11 +17,6 @@ class OAuth2(BaseOAuth2):
     ]
 
     def get_user_details(self, response):
-
-        print 'response ---------------- s'
-        print response
-        print 'response ---------------- e'
-
         """Return user details from Github account"""
         jsonString = dict()
         jsonString['login'] = response.get('user_id')
@@ -30,17 +24,11 @@ class OAuth2(BaseOAuth2):
         jsonString['name'] = response.get('name')
         jsonString['id'] = response.get('id')
 
-        print '***********************'
-        print jsonString
-        print '***********************'
-
         return json.loads(json.dumps(jsonString))
 
     def user_data(self, access_token, *args, **kwargs):
         """Loads user data from service"""
-        print '::: nhi :::  user_data called!!'
-
-        str = self.get_json('https://policy.nhi.go.kr/oauth2/token.do', params={'access_token': access_token})
+        str = self.get_json('https://policy.nhi.go.kr/oauth2/token.do', params={'access_token': access_token, 'grant_type': 'access_token_identify2'})
 
         try:
             return str
@@ -48,20 +36,16 @@ class OAuth2(BaseOAuth2):
             return None
 
     def auth_complete(self, *args, **kwargs):
-
-        print 'auth_complete called1'
-        print self.ACCESS_TOKEN_URL
-        print self.auth_complete_params()
-        print self.auth_headers()
-        print self.ACCESS_TOKEN_METHOD
-        print 'auth_complete called2'
+        data = self.auth_complete_params()
+        data['scope'] = 'http://sso.nhi.go.kr'
 
         response = self.request_access_token(
             self.ACCESS_TOKEN_URL,
-            data=self.auth_complete_params(),
+            data=data,
             headers=self.auth_headers(),
             method=self.ACCESS_TOKEN_METHOD
         )
-        self.process_error(response)
+
+        # self.process_error(response)
         return self.do_auth(response['access_token'], response=response,
                             *args, **kwargs)
