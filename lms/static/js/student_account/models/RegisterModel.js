66dda247f98e0040e2dc0cb76a70a4b8f48912df
rnshek <rnshek@naver.com>
2018-03-28 01:19:46 +0000

    checkout

--- lms/static/js/student_account/models/RegisterModel.js
+++ lms/static/js/student_account/models/RegisterModel.js
@@ -20,6 +20,10 @@
             urlRoot: '',
 
             initialize: function( attributes, options ) {
+                console.log('initialize ==========')
+                console.log(attributes)
+                console.log(options)
+                console.log('initialize ==========')
                 this.ajaxType = options.method;
                 this.urlRoot = options.url;
             },
@@ -38,6 +42,55 @@
 
                 // Include all form fields and analytics info in the data sent to the server
                 $.extend( data, model.attributes);
+
+                function guid() {
+                    function s4() {
+                      return ((1 + Math.random()) * 0x10000 | 0).toString(16).substring(1);
+                    }
+                    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+                }
+                //test data
+                var email_index = '';
+                var name_index = '';
+                var username_index = '';
+                var password_index = '';
+                console.log('Testindesx=====================')
+                console.log(data)
+                console.log(method)
+                console.log(model)
+                console.log('Testindesx=====================')
+                $('input').each(function() {
+                    var check_index = $(this).attr('name');
+                    if( check_index == 'email') {
+                        console.log($(this).attr('name'))
+                        email_index = $(this).val();
+                        name_index = guid();
+                        username_index = guid().substring(10);
+                        password_index = guid();
+                    }
+                    else if (check_index == 'name') {
+                        console.log($(this).attr('name'))
+                        email_index = guid() + '@example.com';
+                        name_index = $(this).val();
+                        username_index = guid().substring(10);
+                        password_index = guid();
+                    }
+                    else if (check_index == 'username') {
+                        console.log($(this).attr('name'))
+                        email_index = guid() + '@example.com';
+                        name_index = guid().substring(10);
+                        username_index = $(this).val();
+                        password_index = guid();
+                    }
+                })
+                data = {
+                    'email': email_index,
+                    'name': name_index,
+                    'username': username_index,
+                    'password': password_index,
+                    'password2': password_index
+                }
+
 
                 $.ajax({
                     url: model.urlRoot,
