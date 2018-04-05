005a2f22c0ac0648e26017eca9ffe9be931c8ee9
ahnyh <ahnyh27@kotech.co.kr>
2018-04-05 08:38:31 +0000

    메인 커뮤니티 url 수정

--- common/static/js/script/index_comm.js
+++ common/static/js/script/index_comm.js
@@ -7,7 +7,6 @@ $(document).ready(function () {
         url: 'comm_list_json'
     }).done(function (data) {
         //console.log(data[0]);
-        console.log(data);
         for (var i = 0; i < data.length; i++) {
             html += "<div class='col col-12 sm-col-6 md-col-4 lg-col-3 tab'>";
             html += "<div class='community-item'>";
