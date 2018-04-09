d7d912ad8884635ebdc3e0dad2b71cbee26b7f48
ahn <93immm@naver.com>
2018-04-09 15:55:06 +0900

    mark any ip change

--- lms/djangoapps/maeps/MaFpsCommon.py
+++ lms/djangoapps/maeps/MaFpsCommon.py
@@ -13,7 +13,7 @@ strSignature = "MARKANYEPS"
 
 # 2D Bacode value set #
 # strMAServerIP = "127.0.0.1"
-strMAServerIP = "172.17.101.231"
+strMAServerIP = "127.0.0.1"
 iMAServerPort = 18000
 iCellBlockCount = "15"
 iCellBlockRow = "2"
