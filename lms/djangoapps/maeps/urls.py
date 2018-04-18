0c8d1de514faad1200ff5dbdd1b0370411c1a93a
ahnyh <ahnyh@kotech.co.kr>
2018-04-18 06:09:17 +0000

    시리즈 관련 views.py 추가

--- lms/djangoapps/maeps/urls.py
+++ lms/djangoapps/maeps/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     url(r'MaGetSession', views.MaGetSession, name='MaGetSession'),
     url(r'MaInstallPage', views.MaInstallPage, name='MaInstallPage'),
     url(r'certificate_print$', 'maeps.views.certificate_print', name='certificate_print'),
+    url(r'series_print$', 'maeps.views.series_print', name='series_print'),
 ]
