1c697057a872cbb2c1413d2da60a9460e9678c9f
h4ppyy <93immm@naver.com>
2019-01-30 08:33:52 +0000

    fix: 회원일괄등록 쉘 출력이 이상하잖앗~

--- add_user.sh
+++ add_user.sh
@@ -1,8 +1,8 @@
 if [ -z "$1" ]
 then
-  echo "user_id is null"
+  echo "user_email is null"
 else
-  echo "user_id is $1"
+  echo "user_email is $1"
 fi
 
 if [ -z "$2" ]
@@ -16,7 +16,7 @@ if [ -z "$3" ]
 then
   echo "user_name is null"
 else
-  echo "user_name is $2"
+  echo "user_name is $3"
 fi
 
-sudo -u www-data /edx/bin/python.edxapp /edx/app/edxapp/edx-platform/manage.py lms --settings aws create_user -p $2 -e $1 -u $3
+sudo -u www-data /edx/bin/python.edxapp /edx/app/edxapp/edx-platform/manage.py lms --settings devstack create_user -p $2 -e $1 -u $3
