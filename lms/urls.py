05dd9eb64da094b1681982818e1032ddbef49493
ahnyh <ahnyh27@kotech.co.kr>
2019-02-18 08:17:39 +0000

    refactor: 묶음강좌 신청기능 완료

--- lms/urls.py
+++ lms/urls.py
@@ -147,6 +147,7 @@ urlpatterns = (
     # series course
     url(r'^series/$', 'community.views.series', name='series'),
     url(r'^series_view/(?P<id>.*?)/about/$', 'community.views.series_about', name='series_about'),
+    url(r'^series_view/(?P<id>.*?)/enroll$', 'community.views.series_enroll', name='series_enroll'),
     url(r'^series_view/(?P<id>.*?)/$', 'community.views.series_view', name='series_view'),
     url(r'^series_print/(?P<id>.*?)/$', 'community.views.series_print', name='series_print'),
     url(r'series_print$', 'maeps.views.series_print', name='series_print'),
