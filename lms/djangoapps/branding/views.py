840b629b0917c2c5129e51fd806e33e0109ba215
h4ppyy <93immm@naver.com>
2019-03-11 17:15:09 +0900

    안진용(님)이 멀티사이트 인덱스 초안을 잡았습니다.

--- lms/djangoapps/branding/views.py
+++ lms/djangoapps/branding/views.py
@@ -27,6 +27,165 @@ from django.views.decorators.csrf import csrf_exempt
 
 log = logging.getLogger(__name__)
 
+#==================================================================================================> login 오버라이딩 시작
+from django.apps import apps as django_apps
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.middleware.csrf import rotate_token
+from django.utils.crypto import constant_time_compare
+from django.utils.module_loading import import_string
+from django.contrib.auth.signals import user_logged_in
+from django.contrib.auth.models import User
+
+SESSION_KEY = '_auth_user_id'
+BACKEND_SESSION_KEY = '_auth_user_backend'
+HASH_SESSION_KEY = '_auth_user_hash'
+REDIRECT_FIELD_NAME = 'next'
+
+def load_backend(path):
+    return import_string(path)()
+
+def _get_backends(return_tuples=False):
+    backends = []
+    for backend_path in settings.AUTHENTICATION_BACKENDS:
+        backend = load_backend(backend_path)
+        backends.append((backend, backend_path) if return_tuples else backend)
+    if not backends:
+        raise ImproperlyConfigured(
+            'No authentication backends have been defined. Does '
+            'AUTHENTICATION_BACKENDS contain anything?'
+        )
+    return backends
+
+
+def get_backends():
+    return _get_backends(return_tuples=False)
+
+def get_user_model():
+
+    try:
+        return django_apps.get_model(settings.AUTH_USER_MODEL, require_ready=False)
+    except ValueError:
+        raise ImproperlyConfigured("AUTH_USER_MODEL must be of the form 'app_label.model_name'")
+    except LookupError:
+        raise ImproperlyConfigured(
+            "AUTH_USER_MODEL refers to model '%s' that has not been installed" % settings.AUTH_USER_MODEL
+        )
+
+def _get_user_session_key(request):
+    # This value in the session is always serialized to a string, so we need
+    # to convert it back to Python whenever we access it.
+    return get_user_model()._meta.pk.to_python(request.session[SESSION_KEY])
+
+def login(request, user, backend=None):
+
+    session_auth_hash = ''
+    if user is None:
+        user = request.user
+    if hasattr(user, 'get_session_auth_hash'):
+        session_auth_hash = user.get_session_auth_hash()
+
+    if SESSION_KEY in request.session:
+        if _get_user_session_key(request) != user.pk or (
+                session_auth_hash and
+                not constant_time_compare(request.session.get(HASH_SESSION_KEY, ''), session_auth_hash)):
+            # To avoid reusing another user's session, create a new, empty
+            # session if the existing session corresponds to a different
+            # authenticated user.
+            request.session.flush()
+    else:
+        request.session.cycle_key()
+
+    try:
+        backend = backend or user.backend
+    except AttributeError:
+        backends = _get_backends(return_tuples=True)
+        if len(backends) == 1:
+            _, backend = backends[0]
+        else:
+            raise ValueError(
+                'You have multiple authentication backends configured and '
+                'therefore must provide the `backend` argument or set the '
+                '`backend` attribute on the user.'
+            )
+
+    request.session[SESSION_KEY] = user._meta.pk.value_to_string(user)
+    request.session[BACKEND_SESSION_KEY] = backend
+    request.session[HASH_SESSION_KEY] = session_auth_hash
+    if hasattr(request, 'user'):
+        request.user = user
+    rotate_token(request)
+    user_logged_in.send(sender=user.__class__, request=request, user=user)
+#==================================================================================================> login 오버라이딩 종료
+
+#==================================================================================================> AES 복호화 함수 시작
+from Crypto.Cipher import AES
+from base64 import b64decode
+from base64 import b64encode
+def decrypt(key, _iv, enc):
+    BLOCK_SIZE = 16  # Bytes
+    pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
+    unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+    enc = b64decode(enc)
+    iv = _iv
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return unpad(cipher.decrypt(enc)).decode('utf8')
+#==================================================================================================> AES 복호화 함수 종료
+
+def multisite_index(request, org, msearch=None):
+
+    print 'org -> ', org
+    print 'msearch -> ', msearch
+
+    user = User.objects.get(pk=10)
+
+    user.backend = 'ratelimitbackend.backends.RateLimitModelBackend'
+    login(request, user)
+
+    # basic logic
+    if request.user.is_authenticated:
+        # Only redirect to dashboard if user has
+        # courses in his/her dashboard. Otherwise UX is a bit cryptic.
+        # In this case, we want to have the user stay on a course catalog
+        # page to make it easier to browse for courses (and register)
+        if configuration_helpers.get_value(
+                'ALWAYS_REDIRECT_HOMEPAGE_TO_DASHBOARD_FOR_AUTHENTICATED_USER',
+                settings.FEATURES.get('ALWAYS_REDIRECT_HOMEPAGE_TO_DASHBOARD_FOR_AUTHENTICATED_USER', True)):
+            #return redirect(reverse('dashboard'))
+            pass
+
+    if settings.FEATURES.get('AUTH_USE_CERTIFICATES'):
+        from openedx.core.djangoapps.external_auth.views import ssl_login
+        # Set next URL to dashboard if it isn't set to avoid
+        # caching a redirect to / that causes a redirect loop on logout
+        if not request.GET.get('next'):
+            req_new = request.GET.copy()
+            req_new['next'] = reverse('dashboard')
+            request.GET = req_new
+        return ssl_login(request)
+
+    enable_mktg_site = configuration_helpers.get_value(
+        'ENABLE_MKTG_SITE',
+        settings.FEATURES.get('ENABLE_MKTG_SITE', False)
+    )
+
+    if enable_mktg_site:
+        marketing_urls = configuration_helpers.get_value(
+            'MKTG_URLS',
+            settings.MKTG_URLS
+        )
+        return redirect(marketing_urls.get('ROOT'))
+
+    domain = request.META.get('HTTP_HOST')
+
+    # keep specialized logic for Edge until we can migrate over Edge to fully use
+    # configuration.
+    if domain and 'edge.edx.org' in domain:
+        return redirect(reverse("signin_user"))
+
+    #  we do not expect this case to be reached in cases where
+    #  marketing and edge are enabled
+    return student.views.management.multisite_index(request, user=request.user)
 
 @csrf_exempt
 def get_org_value(request):
