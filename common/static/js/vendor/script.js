21046b9ebb4f3acd8dbabd0a2640603415ea073c
ahnyh <ahnyh27@kotech.co.kr>
2019-03-22 11:09:38 +0000

    slide css

--- common/static/js/vendor/script.js
+++ common/static/js/vendor/script.js
@@ -3,61 +3,24 @@ var course_slider;
 
 var main_slider;
 var sub_slider;
-var option_4 = {
-  auto: true,
-  autoHover: true,
-  moveSlides: 1,
-  minSlides: 4,
-  maxSlides: 4,
-  slideMargin: 0,
-  slideWidth: 290,
-  speed: 700,
-  pager: false,
-  nextText: '<i class="fa fa-2x fa-chevron-right" aria-hidden="true"></i>',
-  prevText: '<i class="fa fa-2x fa-chevron-left" aria-hidden="true"></i>',
-}
 
-var option_2 = {
-  auto: true,
-  autoHover: true,
-  moveSlides: 1,
-  minSlides: 2,
-  maxSlides: 2,
-  slideMargin: 0,
-  slideWidth: 290,
-  speed: 700,
-  pager: true,
-  nextText: '<i class="fa fa-2x fa-chevron-right" aria-hidden="true"></i>',
-  prevText: '<i class="fa fa-2x fa-chevron-left" aria-hidden="true"></i>',
-}
-
-var option_3 = {
-  auto: true,
-  autoHover: true,
-  moveSlides: 1,
-  minSlides: 3,
-  maxSlides: 3,
-  slideMargin: 0,
-  slideWidth: 290,
-  speed: 700,
-  pager: true,
-  nextText: '<i class="fa fa-2x fa-chevron-right" aria-hidden="true"></i>',
-  prevText: '<i class="fa fa-2x fa-chevron-left" aria-hidden="true"></i>',
-}
-
-var option_1 = {
+var slide_option = {
   auto: true,
   autoHover: true,
   moveSlides: 1,
   minSlides: 1,
   maxSlides: 1,
   slideMargin: 0,
-  slideWidth: 422,
+  slideWidth: 1,
   speed: 700,
-  pager: true,
+  pager: false,
   nextText: '<i class="fa fa-2x fa-chevron-right" aria-hidden="true"></i>',
   prevText: '<i class="fa fa-2x fa-chevron-left" aria-hidden="true"></i>',
-}
+  onSliderLoad: function(){
+    $('.kr01_movie_slider').css({'visibility': 'visible'});
+  }
+};
+
 
 $(window).load(function () {
   window_W = $(window).width();
@@ -76,6 +39,7 @@ $(window).load(function () {
     pager: ($('.kr01_mainslider li').length > 1) ? true : false,
     onSliderLoad: function(currentIndex) {
       $(".slider-txt").html($('.kr01_mainslider li').eq(currentIndex).find("img").attr("alt"));
+      $('.kr01_mainslider_area').css({'visibility': 'visible'});
     },
     onSlideBefore: function($slideElement, oldIndex, newIndex) {
       $(".kr01_mainslider_captions .slider-txt").html($slideElement.find("img").attr("alt"));
@@ -83,42 +47,52 @@ $(window).load(function () {
     // slideWidth: 1730
   });
 
-  var option_list = [option_1, option_2, option_3, option_4];
-
-  for(var i=0; i < option_list.length; i++) {
-    option_list[i].pager = ($("#main_slider li").length > option_list[i].maxSlides) ? true : false;
-  }
-
-  $(window).resize(slide_resize);
   if(window_W <= 471) {
-    main_slider = $("#main_slider").bxSlider(option_1);
-    sub_slider = $("#sub_slider").bxSlider(option_1);
+    slide_option.minSlides = 1;
+    slide_option.maxSlides = 1;
+    slide_option.slideWidth = 422;
   } else if(window_W < 720 && window_W > 471){
-    main_slider = $("#main_slider").bxSlider(option_2);
-    sub_slider = $("#sub_slider").bxSlider(option_2);
+    slide_option.minSlides = 2;
+    slide_option.maxSlides = 2;
+    slide_option.slideWidth = 290;
   } else if(window_W >= 720 && window_W < 991) {
-    main_slider = $("#main_slider").bxSlider(option_3);
-    sub_slider = $("#sub_slider").bxSlider(option_3);
+    slide_option.minSlides = 3;
+    slide_option.maxSlides = 3;
+    slide_option.slideWidth = 290;
   } else {
-    main_slider = $("#main_slider").bxSlider(option_4);
-    sub_slider = $("#sub_slider").bxSlider(option_4);
+    slide_option.minSlides = 4;
+    slide_option.maxSlides = 4;
+    slide_option.slideWidth = 290;
+
   }
+
+  slide_option.pager = ($("#main_slider li").length > slide_option.maxSlides) ? true : false;
+
+  main_slider = $("#main_slider").bxSlider(slide_option);
+  sub_slider = $("#sub_slider").bxSlider(slide_option);
+  $(window).resize(slide_resize);
 });
 
 function slide_resize() {
   window_W = $(window).width();
   if(window_W <= 471){
-    main_slider.reloadSlider(option_1);
-    sub_slider.reloadSlider(option_1);
+    slide_option.minSlides = 1;
+    slide_option.maxSlides = 1;
+    slide_option.slideWidth = 422;
   }else if (window_W < 720 && window_W > 471) {
-    main_slider.reloadSlider(option_2);
-    sub_slider.reloadSlider(option_2);
+    slide_option.minSlides = 2;
+    slide_option.maxSlides = 2;
+    slide_option.slideWidth = 290;
   } else if(window_W >= 720 && window_W < 991) {
-    main_slider.reloadSlider(option_3);
-    sub_slider.reloadSlider(option_3);
+    slide_option.minSlides = 3;
+    slide_option.maxSlides = 3;
+    slide_option.slideWidth = 290;
   } else {
-    main_slider.reloadSlider(option_4);
-    sub_slider.reloadSlider(option_4);
+    slide_option.minSlides = 4;
+    slide_option.maxSlides = 4;
+    slide_option.slideWidth = 290;
   }
+  main_slider.reloadSlider(slide_option);
+  sub_slider.reloadSlider(slide_option);
 
 }
