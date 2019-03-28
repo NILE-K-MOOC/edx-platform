9d23aadf790255fc641fd9daa062af1dbd76e4a6
h4ppyy <93immm@naver.com>
2019-03-28 18:53:32 +0900

    마크애니 아이피 변경

--- lms/djangoapps/maeps/MaFpsCommon.py
+++ lms/djangoapps/maeps/MaFpsCommon.py
@@ -12,9 +12,9 @@ strPVersion = "25131"
 strSignature = "MARKANYEPS"
 
 # 2D Bacode value set #
-strMAServerIP = "172.17.101.228"
+#strMAServerIP = "172.17.101.228"
 #strMAServerIP = 'docker.for.mac.localhost'
-#strMAServerIP = "203.235.44.154"
+strMAServerIP = "203.235.44.154"
 #iMAServerPort = 8888
 iMAServerPort = 18000
 iCellBlockCount = "15"
