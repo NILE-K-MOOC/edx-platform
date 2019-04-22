d60400f4eef5cb6dbef5388ae5a82bb91cc72eff
ahnyh <ahnyh27@kotech.co.kr>
2019-04-22 11:02:32 +0900

    fix: 쪽지화면 디자인 깨짐 수정

--- common/static/js/script/comm_memo.js
+++ common/static/js/script/comm_memo.js
@@ -17,8 +17,6 @@ $(document).ready(function () {
         if (e.keyCode == 13)
             search(1);
     });
-
-    $(".board-search-box").append('<div style="float: left; margin-top: 20px; margin-left: 5px;">알림은 발송일로부터 6개월이 지나면 자동 삭제됩니다.</div>');
 });
 
 Date.prototype.yyyymmdd = function () {
