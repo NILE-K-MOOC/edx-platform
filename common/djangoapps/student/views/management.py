5ed34fe54a30c6e24cdecb2092944b6886e5a44f
redukyo <redukyo@gmail.com>
2019-05-10 11:07:32 +0900

    회원가입시 lifelongedu, allprovider, nhi, nec 로그인연계를 이용하면 비밀번호를 직접 입력하도록 수정

--- common/djangoapps/student/views/management.py
+++ common/djangoapps/student/views/management.py
@@ -1150,7 +1150,9 @@ def create_account_with_params(request, params):
 
     is_third_party_auth_enabled = third_party_auth.is_enabled()
 
-    if is_third_party_auth_enabled and (pipeline.running(request) or third_party_auth_credentials_in_api):
+    # pipeline 에서 비밀번호를 파라미터로 쓰는 내용을 하드 코딩필요
+    # params 에 password2 가 있다면 있는 내용을 사용함
+    if is_third_party_auth_enabled and (pipeline.running(request) or third_party_auth_credentials_in_api) and 'password2' not in params:
         params["password"] = generate_password()
 
     # in case user is registering via third party (Google, Facebook) and pipeline has expired, show appropriate
