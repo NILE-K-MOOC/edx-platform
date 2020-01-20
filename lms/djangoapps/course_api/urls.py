fcdd75da17ba45503ebe657420a96a5ea122b26d
redukyo <redukyo@gmail.com>
2020-01-20 18:10:59 +0900

    공공데이터 파라미터명 변경

--- lms/djangoapps/course_api/urls.py
+++ lms/djangoapps/course_api/urls.py
@@ -7,9 +7,9 @@ from django.conf.urls import include, url
 from .views import CourseDetailView, CourseListView
 
 urlpatterns = [
-    url(r'^v1/course/list/$', CourseListView.as_view(), name="course-list"),
+    url(r'^v1/course/list/$', CourseListView.as_view(), name="courseList"),
     url(r'^v1/courses/$', CourseListView.as_view(), name="course-list"),
-    url(r'^v1/course/detail/$', CourseDetailView.as_view(), name="course-view"),
+    url(r'^v1/course/detail/$', CourseDetailView.as_view(), name="courseDetail"),
     url(r'^v1/courses/{}'.format(settings.COURSE_KEY_PATTERN), CourseDetailView.as_view(), name="course-detail"),
     url(r'', include('course_api.blocks.urls'))
 ]
