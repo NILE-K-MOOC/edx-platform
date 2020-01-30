c78925ebbe64160f93cdbc6e8c2d438772095406
redukyo <redukyo@gmail.com>
2020-01-30 10:39:05 +0900

    오토에버 연계의 상세페이지 기능 개발 중

--- lms/djangoapps/kotech_community/views.py
+++ lms/djangoapps/kotech_community/views.py
@@ -6,7 +6,6 @@ import MySQLdb as mdb
 import sys
 import re
 import os.path
-import datetime
 from django.conf import settings
 from django.http import (HttpResponse, HttpResponseBadRequest, HttpResponseForbidden, HttpRequest)
 from django.shortcuts import redirect
@@ -16,17 +15,13 @@ from util.json_request import JsonResponse
 from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 from django.core.serializers.json import DjangoJSONEncoder
 from django.core.mail import send_mail
-from django.db import models, connections
 from django.forms.models import model_to_dict
 from django.core.paginator import Paginator
 from django.db.models import Q
-from datetime import timedelta
-from pytz import timezone
 from django.db import connections
 from django.core.urlresolvers import reverse
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import AnonymousUser
+from models import *
 
 import mimetypes
 import urllib
@@ -40,63 +35,6 @@ def replace_all(string):
     return string
 
 
-class TbBoard(models.Model):
-    board_id = models.AutoField(primary_key=True)
-    head_title = models.CharField(max_length=50, blank=True, null=True)
-    subject = models.TextField()
-    content = models.TextField(blank=True, null=True)
-    reg_date = models.DateTimeField()
-    mod_date = models.DateTimeField()
-    # section
-    # N : notice, F: faq, K: k-mooc news, R: reference
-    section = models.CharField(max_length=10)
-    use_yn = models.CharField(max_length=1)
-    odby = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'tb_board'
-        app_label = 'tb_board'
-
-
-class TbAttach(models.Model):
-    group_name = models.CharField(max_length=255, blank=True, null=True)
-    group_id = models.CharField(max_length=255, blank=True, null=True)
-    real_name = models.CharField(max_length=255, blank=True, null=True)
-    save_name = models.CharField(max_length=255, blank=True, null=True)
-    ext = models.CharField(max_length=10, blank=True, null=True)
-    real_size = models.IntegerField(blank=True, null=True)
-    save_size = models.CharField(max_length=255, blank=True, null=True)
-    save_path = models.CharField(max_length=255, blank=True, null=True)
-    use_yn = models.IntegerField(default=True)
-    regist_id = models.IntegerField(blank=True, null=True)
-    regist_date = models.DateTimeField(blank=True, null=True, auto_now_add=True)
-    delete_date = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'tb_attach'
-        app_label = 'tb_attach'
-
-
-class TbBoardAttach(models.Model):
-    attatch_id = models.AutoField(primary_key=True)
-    # board_id = models.ForeignKey('TbBoard', on_delete=models.CASCADE, related_name='attaches', null=True)
-    board_id = models.IntegerField(11)
-    attach_file_path = models.CharField(max_length=255)
-    attatch_file_name = models.CharField(max_length=255)
-    attach_org_name = models.CharField(max_length=255, blank=True, null=True)
-    attatch_file_ext = models.CharField(max_length=50, blank=True, null=True)
-    attatch_file_size = models.CharField(max_length=50, blank=True, null=True)
-    attach_gubun = models.CharField(max_length=20, blank=True, null=True)
-    del_yn = models.CharField(max_length=1)
-    regist_id = models.IntegerField(blank=True, null=True)
-    regist_date = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'tb_board_attach'
-        app_label = 'tb_board_attach'
 
 
 @ensure_csrf_cookie
