c7c17b84181a370d1b77e34744fdc23fcb5b4fe8
redukyo <redukyo@gmail.com>
2020-03-05 18:11:14 +0900

    공공데이터 확인용 api 매핑 url 추가

--- lms/djangoapps/course_api/urls.py
+++ lms/djangoapps/course_api/urls.py
@@ -7,8 +7,10 @@ from django.conf.urls import include, url
 from .views import CourseDetailView, CourseListView
 
 urlpatterns = [
+    url(r'^v1/course/list$', CourseListView.as_view(), name="courseList2"),
     url(r'^v1/course/list/$', CourseListView.as_view(), name="courseList"),
     url(r'^v1/courses/$', CourseListView.as_view(), name="course-list"),
+    url(r'^v1/course/detail$', CourseDetailView.as_view(), name="courseDetail2"),
     url(r'^v1/course/detail/$', CourseDetailView.as_view(), name="courseDetail"),
     url(r'^v1/courses/{}'.format(settings.COURSE_KEY_PATTERN), CourseDetailView.as_view(), name="course-detail"),
     url(r'', include('course_api.blocks.urls'))
