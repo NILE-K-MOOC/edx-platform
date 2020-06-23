1c9913aedc16512508dec14f2de0764f988e7c09
92hoy <ho_9209@naver.com>
2020-06-23 15:47:30 +0900

    css / lms-static

--- lms/lib/comment_client/utils.py
+++ lms/lib/comment_client/utils.py
@@ -77,6 +77,7 @@ def perform_request(method, url, data_or_params=None, raw=False,
         metric_tags.append(u'result:success')
 
     if 200 < response.status_code < 500:
+        print "response.status_code",response.status_code
         raise CommentClientRequestError(response.text, response.status_code)
     # Heroku returns a 503 when an application is in maintenance mode
     elif response.status_code == 503:
