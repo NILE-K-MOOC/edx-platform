167413a3dce6aab2a343d3276d2ddfae73238ecb
redukyo <redukyo@gmail.com>
2020-12-28 14:07:46 +0900

    forum error test

--- common/static/common/js/discussion/views/discussion_inline_view.js
+++ common/static/common/js/discussion/views/discussion_inline_view.js
@@ -36,7 +36,7 @@
                 this.startHeader = 4; // Start the header levels at H<startHeader>
             }
 
-            var l = window.location.href;
+            var loc = window.location.href;
             match = this.page_re.exec(l.replace(/</g, "").replace(/>/g, ""));
             if (match) {
                 var tmp = match[1];
