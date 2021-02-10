78b135d9998cfc9c500c1aaeb42ca3361722c616
redukyo <redukyo@gmail.com>
2021-02-10 12:30:33 +0900

    집콕강좌 설정 확인

--- lms/static/js/discovery/views/refine_sidebar.js
+++ lms/static/js/discovery/views/refine_sidebar.js
@@ -509,6 +509,13 @@
                             break;
                         case 'home_course_step':
                             k = 'home_course_step';
+                            v = v;
+                            if( v == '1')
+                                t = 'home_course_step_1';
+                            else if( v == '2')
+                                t = 'home_course_step_2';
+                            else if( v == '3')
+                                t = 'home_course_step_3';
                             break;
                     }
 
