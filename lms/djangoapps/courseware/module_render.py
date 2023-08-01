48110691d9dfbbfed64323529a2e8b766c9e1a74
ubion <kmooc.kr@gmail.com>
2023-08-01 16:12:24 +0900

    [230801_01] 영문자막에서 <> 특수기호 생략 문제 - www쪽 - 2

--- lms/djangoapps/courseware/module_render.py
+++ lms/djangoapps/courseware/module_render.py
@@ -1070,15 +1070,6 @@ def _invoke_xblock_handler(request, course_id, usage_id, handler, suffix, course
         try:
             with tracker.get_tracker().context(tracking_context_name, tracking_context):
                 resp = instance.handle(handler, req, suffix)
-                if resp.content_type == "application/json":
-                    tmparray = []
-                    for value in resp.app_iter:
-                        json_object = json.loads(value)
-                        for subvalue in json_object["text"]:
-                            tmparray.append(subvalue.replace("<","&lt;"))
-                        json_object["text"] = tmparray
-                    resp.app_iter = json.dumps(json_object)
-
                 if suffix == 'problem_check' \
                         and course \
                         and getattr(course, 'entrance_exam_enabled', False) \
@@ -1105,6 +1096,15 @@ def _invoke_xblock_handler(request, course_id, usage_id, handler, suffix, course
         except Exception:
             log.exception("error executing xblock handler")
             raise
+
+        if resp.content_type == "application/json":
+            tmparray = []
+            for value in resp.app_iter:
+                json_object = json.loads(value)
+                for subvalue in json_object["text"]:
+                    tmparray.append(subvalue.replace("<","&lt;"))
+                json_object["text"] = tmparray
+            resp.app_iter = json.dumps(json_object)
 
     return webob_to_django_response(resp)
 
