bbd25a4155d9355b0cc924d6b25ac0719f1c089e
ubion <kmooc.kr@gmail.com>
2023-08-02 13:16:30 +0900

    [230801_01] 영문자막에서 <> 특수기호 생략 문제 - www쪽 back

--- lms/djangoapps/courseware/module_render.py
+++ lms/djangoapps/courseware/module_render.py
@@ -1097,14 +1097,14 @@ def _invoke_xblock_handler(request, course_id, usage_id, handler, suffix, course
             log.exception("error executing xblock handler")
             raise
 
-        if resp.content_type == "application/json":
-            tmparray = []
-            for value in resp.app_iter:
-                json_object = json.loads(value)
-                for subvalue in json_object["text"]:
-                    tmparray.append(subvalue.replace("<","&lt;"))
-                json_object["text"] = tmparray
-            resp.app_iter = json.dumps(json_object)
+        # if resp.content_type == "application/json":
+        #     tmparray = []
+        #     for value in resp.app_iter:
+        #         json_object = json.loads(value)
+        #         for subvalue in json_object["text"]:
+        #             tmparray.append(subvalue.replace("<","&lt;"))
+        #         json_object["text"] = tmparray
+        #     resp.app_iter = json.dumps(json_object)
 
     return webob_to_django_response(resp)
 
