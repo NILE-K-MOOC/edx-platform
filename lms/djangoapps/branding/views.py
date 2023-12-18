03f7fe9a88b79a74009b8c38428463b37c7a7673
ubion <kmooc.kr@gmail.com>
2023-12-18 15:43:11 +0900

    자막 뽑기 - 32

--- lms/djangoapps/branding/views.py
+++ lms/djangoapps/branding/views.py
@@ -2340,8 +2340,8 @@ def vodfile_move_one_nofile(request):
         m_port = settings.CONTENTSTORE.get('DOC_STORE_CONFIG').get('port')
         client = MongoClient(m_host, m_port)
         db = client.edxapp
-        #mdlcon = mdb.connect('192.168.1.245', 'openlms', 'dhvms@23gkrTmq', 'openlms', charset='utf8')
-        mdlcon = mdb.connect('118.67.152.82', 'root', 'anzmRoqkf@2022', 'edxapp', charset='utf8')
+        mdlcon = mdb.connect('192.168.1.245', 'openlms', 'dhvms@23gkrTmq', 'openlms', charset='utf8')
+        #mdlcon = mdb.connect('118.67.152.82', 'root', 'anzmRoqkf@2022', 'edxapp', charset='utf8')
 
         mdlcur = mdlcon.cursor()
         for cblock in coursetmp:
